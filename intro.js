ee2fefb5cf6b6c5fdb0f1f15b322650edc23aa71
spaceowel <42473779+spaceowel@users.noreply.github.com>
2018-11-02 18:42:17 +0200

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,5 +1,6 @@
 var name = document.getElementById("name");
 var start = document.getElementById("start");
+var info = document.getElementById("info");
 start.addEventListener("click", function() {
-  
+  info.className = "hide";
 });
